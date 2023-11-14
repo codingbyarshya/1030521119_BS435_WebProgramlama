f67d2c1d474a86af9ad92da7fae418c654607573
codingbyarshya <117349670+codingbyarshya@users.noreply.github.com>
2023-11-14 20:55:07 +0300

    Update NumberGuessGame.js

--- src/NumberGuessGame.js
+++ src/NumberGuessGame.js
@@ -7,7 +7,9 @@ class NumberGuessGame extends Component {
             targetNumber: Math.floor(Math.random() * 100) + 1,
             guess: '',
             message: '',
-            attempts: 0,
+            playerAttempts: { 1: 0, 2: 0 },
+            currentPlayer: 1,
+            isTwoPlayerMode: false,
         };
     }
 
@@ -16,34 +18,73 @@ class NumberGuessGame extends Component {
     };
 
     handleGuess = () => {
-        const { targetNumber, guess, attempts } = this.state;
+        const { targetNumber, guess, playerAttempts, currentPlayer, isTwoPlayerMode } = this.state;
 
         if (isNaN(guess)) {
             this.setState({ message: 'Please enter a valid number' });
         } else {
-            const newAttempts = attempts + 1;
+            const newAttempts = playerAttempts[currentPlayer] + 1;
+            const playerLabel = isTwoPlayerMode ? `Player ${currentPlayer}, ` : '';
+
             if (parseInt(guess, 10) === targetNumber) {
-                this.setState({ message: `Congratulations! You guessed the number ${targetNumber} in ${newAttempts} attempts.` });
+                this.setState({
+                    message: `${playerLabel}Congratulations! You guessed the number ${targetNumber} in ${newAttempts} attempts.`,
+                    playerAttempts: { ...playerAttempts, [currentPlayer]: newAttempts },
+                });
+            } else if (newAttempts >= 10) {
+                this.setState({
+                    message: `${playerLabel}Sorry, you've reached the maximum number of attempts. The correct number was ${targetNumber}.`,
+                    playerAttempts: { ...playerAttempts, [currentPlayer]: newAttempts },
+                });
             } else if (parseInt(guess, 10) < targetNumber) {
-                this.setState({ message: 'Too low! Try again.', attempts: newAttempts });
+                this.setState({
+                    message: `${playerLabel}Too low! Try again.`,
+                    playerAttempts: { ...playerAttempts, [currentPlayer]: newAttempts },
+                });
             } else {
-                this.setState({ message: 'Too high! Try again.', attempts: newAttempts });
+                this.setState({
+                    message: `${playerLabel}Too high! Try again.`,
+                    playerAttempts: { ...playerAttempts, [currentPlayer]: newAttempts },
+                });
+            }
+
+            if (isTwoPlayerMode) {
+                const nextPlayer = currentPlayer === 1 ? 2 : 1;
+                this.setState({ currentPlayer: nextPlayer });
             }
         }
+    };
+
+    handleRestart = () => {
+        this.setState({
+            targetNumber: Math.floor(Math.random() * 100) + 1,
+            guess: '',
+            message: '',
+            playerAttempts: { 1: 0, 2: 0 },
+            currentPlayer: 1,
+        });
+    };
+
+    toggleGameMode = () => {
+        this.setState((prevState) => ({ isTwoPlayerMode: !prevState.isTwoPlayerMode }));
     };
 
     render() {
         return (
             <div>
                 <h1>Number Guessing Game</h1>
+                <button onClick={this.toggleGameMode}>
+                    {this.state.isTwoPlayerMode ? 'Switch to Single Player Mode' : 'Switch to Two Player Mode'}
+                </button>
                 <p>{this.state.message}</p>
                 <input
                     type="number"
-                    placeholder="Enter your guess"
+                    placeholder={`${this.state.isTwoPlayerMode ? `Player ${this.state.currentPlayer}, ` : ''}enter your guess`}
                     value={this.state.guess}
                     onChange={this.handleInputChange}
                 />
                 <button onClick={this.handleGuess}>Guess</button>
+                <button onClick={this.handleRestart}>Restart</button>
             </div>
         );
     }
