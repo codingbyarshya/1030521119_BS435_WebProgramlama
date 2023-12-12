1a0400b4be917639edc05f45fc206004301fcab3
codingbyarshya <117349670+codingbyarshya@users.noreply.github.com>
2023-12-12 20:33:37 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NumberGuessGame from './NumberGuessGame';
 import './App.css';
 import number from './number.png';
 
 function App() {
-    return (
-        <div>
-            <NumberGuessGame />
-            <img src={number} alt="Number" />
-        </div>
-    );
+  const [nightMode, setNightMode] = useState(false);
+
+  const toggleNightMode = () => {
+    setNightMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <div className={`app-container ${nightMode ? 'night-mode' : ''}`}>
+      <NumberGuessGame />
+      <img src={number} alt="Number" className={`number-image ${nightMode ? 'night-mode' : ''}`} />
+      <button onClick={toggleNightMode} className={`toggle-night-mode ${nightMode ? 'night-mode' : ''}`}>
+        {nightMode ? 'Day Mode' : 'Night Mode'}
+      </button>
+    </div>
+  );
 }
 
 export default App;
